f8d2e8f8bb4b2c84ff561c1a66868d2aec21957a
Christian Welzel <139749352+welzel-online@users.noreply.github.com>
2025-09-23 18:54:00 +0200

    - Bug fix: Corrected the version-git.in. The FILEVER and PRODUCTVER only accept numbers. - Bumped the minimal version of cmake to 3.10.

--- src/version.h
+++ src/version.h
@@ -12,19 +12,19 @@
 
 
 /******************************************************************* Defines **/
-#define TRACK0_REVISION     "0.0.1 (0000000)"
+#define TRACK0_REVISION     "0.0.1 (3cdd5d8)"
 
-#define FILEVER             0,0,1,0000000
-#define PRODUCTVER          0,0,1,0000000
-#define STRFILEVER          "0, 0, 1, 0000000\0"
-#define STRPRODUCTVER       "0, 0, 1, 0000000\0"
-#define STRVERDATE          "01.01.1970\0"
+#define FILEVER             0,0,1,0
+#define PRODUCTVER          0,0,1,0
+#define STRFILEVER          "0, 0, 1, 3cdd5d8\0"
+#define STRPRODUCTVER       "0, 0, 1, 3cdd5d8\0"
+#define STRVERDATE          "21.09.2025\0"
 
-#define CM_VERMAJOR         0
-#define CM_VERMINOR         0
-#define CM_VERMICRO         1
-#define CM_VERBUILD         0000000
-#define CM_VERDATE          "1970/01/01 01:00:00"
+// #define CM_VERMAJOR         0
+// #define CM_VERMINOR         0
+// #define CM_VERMICRO         1
+// #define CM_VERBUILD         3cdd5d8
+// #define CM_VERDATE          "2025/09/21 12:31:18"
 
 
 #endif
